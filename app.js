f6d94c8a2729cb2d5d5ee0ed21723ea75a3695ca
AlexDanicic <alex.danicic@gmail.com>
2018-09-13 22:17:14 +0100

    first door finished

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 let doorImage1 = document.getElementById('#door1');
+let botDoorPath = "https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/robot.svg";
 
 door1.onclick = () => {
     // console.log('test');
+    door1.src= botDoorPath;
 };
