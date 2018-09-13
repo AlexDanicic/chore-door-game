9f6cbedf044e6a718be79774c271d5dfc8d317c7
AlexDanicic <alex.danicic@gmail.com>
2018-09-13 22:47:38 +0100

    doors added and they are work onclick

--- app.js
+++ app.js
@@ -1,7 +1,25 @@
 let doorImage1 = document.getElementById('#door1');
+
+let doorImage2 = document.getElementById('#door2');
+
+let doorImage3 = document.getElementById('#door3');
+
+
 let botDoorPath = "https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/robot.svg";
+
+let beachDoorPath = "https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/beach.svg";
+
+let spaceDoorPath = "https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/space.svg";
 
 door1.onclick = () => {
     // console.log('test');
-    door1.src= botDoorPath;
+    door1.src = botDoorPath;
+};
+
+door2.onclick = () => {
+    door2.src = beachDoorPath;
+};
+
+door3.onclick = () => {
+    door3.src = spaceDoorPath;
 };
